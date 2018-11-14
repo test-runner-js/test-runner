ae879fd9de67a9df7007ad924b4ea83ae4d1393e
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-11-14 23:53:10 +0000

    skip

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const TestRunner = require('test-runner')
 
 class TAPView {
   start (count) {
+    console.log('TAP version 13')
     console.log(`1..${count}`)
   }
-  testPass (test) {
-    console.log(`ok ${test.id} ${test.name}`)
+  testPass (test, result) {
+    console.log(`ok ${test.id} ${test.name} ${result || ''}`)
   }
-  testFail (test) {
+  testFail (test, err) {
     console.log(`not ok ${test.id} ${test.name}`)
+    console.log(err)
+  }
+  testSkip (test) {
+    console.log(`ok ${test.id} ${test.name} # SKIP`)
   }
 }
-
 const runner = new TestRunner({ view: new TAPView() })
 
 module.exports = runner
