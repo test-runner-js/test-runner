09fb5ed4f362705ccb51cfa1250e55325040461f
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-03-03 21:39:26 +0000

    runner fail exit code

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 const TestRunner = require('../')
 const cli = new TestRunner()
-cli.start().catch(err => console.error('RUNNER FAIL', err))
+cli.start().catch(err => {
+	console.error(require('util').inspect(err, { depth: 6, colors: true }))
+	process.exitCode = 1
+})
